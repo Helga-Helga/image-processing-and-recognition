e59d5416282d6e39b53fc47b9c469078caab5c70
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2019-11-13 23:38:52 +0200

    Intersect rectangles

--- lab2-local_normalization/image_normalization.py
+++ lab2-local_normalization/image_normalization.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from numpy import clip
+from statistics import mean
 
 from integral_image import (
     compute_integral_image,
@@ -7,8 +8,27 @@ from integral_image import (
 )
 
 
+def average_intensity(intensities):
+    """Calculation of average intensity value among list elements
+
+    Parameters
+    ----------
+    intensities : list of integers
+        list of intensities for pixel
+
+    Returns
+    -------
+    int
+        Average intensity from the list
+    """
+    if len(intensities) == 1:
+        return clip(intensities, 0, 255)
+    return clip(round(mean(intensities)), 0, 255)
+
+
 def normalize_image(image, integral_image, integral_image_square,
-                    grid_size_h, grid_size_v, new_mean, new_dispersion):
+                    grid_size_h, grid_size_v, intersection_area,
+                    new_mean, new_dispersion):
     """Local brightness normalizaiton of rectangular blocks of image
     by new mean and dispersion values using image integral representation
 
@@ -20,8 +40,12 @@ def normalize_image(image, integral_image, integral_image_square,
         Integral representation of image
     integral_image_square: numpy 2d array
         Integral representation of image, where intensities are squared
-    grid_size: int
-        Size in pixels of rectangle (currently square)
+    grid_size_h: int
+        Height in pixels of rectangle
+    grid_size_v: int
+        Width in pixels of rectangle
+    intersection_area: int
+        Width and height of intersected area between two neighbor rectangles
     new_mean:
         Mean value for intensities in normalized image
     new_dispersion:
@@ -32,10 +56,23 @@ def normalize_image(image, integral_image, integral_image_square,
     numpy 2d array
         Normalized image
     """
-    normalized_image = image.copy()
     height, width = image.shape
-    for i in range(0, height, grid_size_v):
-        for j in range(0, width, grid_size_h):
+
+    # create matrix of lists for normalized intensities
+    # new intensity for a pixel will be
+    # an average intensity of corresponding list
+    normalized_intensities = []
+    for i in range(height):
+        normalized_intensities.append([])
+        for j in range(width):
+            normalized_intensities[i].append([])
+
+    for i in range(0, height, grid_size_v - intersection_area):
+        if i + grid_size_v >= height:
+            i = height - grid_size_v
+        for j in range(0, width, grid_size_h - intersection_area):
+            if j + grid_size_h >= width:
+                j = width - grid_size_h
             window = [j, i, grid_size_h, grid_size_v]
             mean = window_sum(
                 integral_image, window) / (grid_size_h * grid_size_v)
@@ -46,9 +83,16 @@ def normalize_image(image, integral_image, integral_image_square,
             for pixel_i in range(i, i + grid_size_v):
                 for pixel_j in range(j, j + grid_size_h):
                     if dispersion == 0.0:
-                        normalized_image[pixel_i, pixel_j] = new_mean
+                        normalized_intensities[pixel_i][pixel_j].append(
+                            new_mean)
                     else:
-                        normalized_image[pixel_i, pixel_j] = \
-                            clip((image[pixel_i, pixel_j] - mean) * \
-                            new_dispersion / dispersion + new_mean, 0, 255)
+                        normalized_intensities[pixel_i][pixel_j].append(
+                            (image[pixel_i, pixel_j] - mean) *
+                            new_dispersion / dispersion + new_mean)
+
+    normalized_image = image.copy()
+    for i in range(height):
+        for j in range(width):
+            normalized_image[i, j] = average_intensity(
+                normalized_intensities[i][j])
     return normalized_image
