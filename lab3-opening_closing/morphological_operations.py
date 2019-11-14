7c1016c3363ff366cbd6707c42e009dac1b078fe
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2019-11-14 23:28:08 +0200

    Implement erosion of binary image

--- lab3-opening_closing/morphological_operations.py
+++ lab3-opening_closing/morphological_operations.py
@@ -24,6 +24,36 @@ def get_filter_indexes(height, width):
     return indexes
 
 
+def expand_image(image, height, width):
+    """Add white borders around the image
+
+    Parameters
+    ----------
+    image : numpy 2d array
+        Input binary image
+    height: int
+        Number of rows to add at the beginning and at the end
+    width: int
+        Number of columns to add at the beginning and at the end
+
+    Returns
+    -------
+    numpy 2d array
+        Expanded image with white border
+    """
+    expanded_image = concatenate(
+        ([[1] * image.shape[1]] * width, image), axis=0)
+    expanded_image = concatenate(
+        (expanded_image, [[1] * image.shape[1]] * width), axis=0)
+    dilated_expanded_imageimage = concatenate(
+        ([[1] * height] * expanded_image.shape[0], expanded_image),
+        axis=1)
+    expanded_image = concatenate(
+        (expanded_image, [[1] * height] * expanded_image.shape[0]),
+        axis=1)
+    return expanded_image
+
+
 def dilation(image, structural_element):
     """Binary image dilation
 
@@ -47,16 +77,7 @@ def dilation(image, structural_element):
     indexes = get_filter_indexes(height, width)
 
     # add white borders
-    dilated_image = concatenate(
-        ([[1] * image.shape[1]] * (width // 2), image), axis=0)
-    dilated_image = concatenate(
-        (dilated_image, [[1] * image.shape[1]] * (width // 2)), axis=0)
-    dilated_image = concatenate(
-        ([[1] * (height // 2)] * dilated_image.shape[0], dilated_image),
-        axis=1)
-    dilated_image = concatenate(
-        (dilated_image, [[1] * (height // 2)] * dilated_image.shape[0]),
-        axis=1)
+    dilated_image = expand_image(image, height // 2, width // 2)
 
     # go through image pixels
     for i in range(height // 2, image.shape[0] - height + 1):
@@ -72,3 +93,45 @@ def dilation(image, structural_element):
 
                             dilated_image[im_i, im_j] = center_pixel
     return dilated_image
+
+
+def erosion(image, structural_element):
+    """Binary image erosion
+
+    Parameters
+    ----------
+    image : numpy 2d array
+        Input binary image
+    structural_element: numpy 2d array
+        Binary filter with odd side sizes
+
+    Returns
+    -------
+    numpy 2d array
+        Erosed image by structural element
+    """
+    height, width = structural_element.shape
+
+    # get center pixel intensity of structural element
+    center_pixel = structural_element[height // 2, width // 2]
+
+    indexes = get_filter_indexes(height, width)
+
+    # add white borders
+    erosed_image = expand_image(image, height // 2, width // 2)
+
+    # go through image pixels
+    for i in range(height // 2, image.shape[0] - height + 1):
+        for j in range(width // 2, image.shape[1] - width + 1):
+            number_of_errors = 0
+            # go through filter pixels
+            for s_i in range(height):
+                for s_j in range(width):
+                    # get corresponding image pixel coordinate
+                    im_i = i + int(indexes[s_i, s_j, 0])
+                    im_j = j + int(indexes[s_i, s_j, 1])
+                    if image[im_i, im_j] != structural_element[s_i, s_j]:
+                        number_of_errors += 1
+            if number_of_errors > 0:
+                erosed_image[i, j] = 1 - center_pixel
+    return erosed_image
